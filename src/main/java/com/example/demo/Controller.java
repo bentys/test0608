a59f8dbfe6c513ba925bafe14095a5ecf5eb2666
左子穆 <xct15990138050@xct15990138050deMacBook-Pro.local>
2018-06-04 22:00:57 +0800

    1

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -5,18 +5,51 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static android.R.attr.name;
+
 @RestController
-@RequestMapping("/como/")
+
 public class Controller {
-	@RequestMapping(value = "say",method = RequestMethod.GET)
-	public String say(){
-		return "HEll Spring Hsit";
+
+
+	@RequestMapping(value = "users",method = RequestMethod.GET)
+	public @ResponseBody Object users(){
+		List<String> userList=new ArrayList<>();
+		userList.add("tom");
+		userList.add("marry");
+		userList.add("jack");
+		return userList;
+	}
+
+
+	@RequestMapping(value ="login", method = RequestMethod.POST)
+
+		public @ResponseBody Object login(String name,String pwd){
+
+			Map<String,Object> map=new HashMap<>();
+
+			if ("123".equals(pwd)&&"jack".equals(name)){
+				map.put("status",0);
+			}else{
+
+				map.put("status",-1);
+			}
+
+			return map;
+
+		}
 
 	}
 
-}
+
 
 
 
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,35 @@
 package com.example.demo;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Let's.........");
+
+			String[] baenNameas = ctx.getBeanDefinitionNames();
+			Arrays.sort(baenNameas);
+			for (String beanName : baenNameas) {
+
+				System.out.println(beanName);
+			}
+
+
+		};
 	}
 }
