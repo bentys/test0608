7575cb23326e9d0a288ee0b15e1fd90e4a61503f
左子穆 <xct15990138050@xct15990138050deMacBook-Pro.local>
2018-06-05 21:31:02 +0800

    提交内容

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -14,18 +14,28 @@ import java.util.List;
 import java.util.Map;
 
 import static android.R.attr.name;
+import static android.R.attr.publicKey;
 
 @RestController
 
 public class Controller {
 
-
+	int x=1;
+	int y=1;
+	int z=0;
+	int p=0;
 	@RequestMapping(value = "users",method = RequestMethod.GET)
 	public @ResponseBody Object users(){
 		List<String> userList=new ArrayList<>();
 		userList.add("tom");
 		userList.add("marry");
 		userList.add("jack");
+
+
+
+		System.out.println("get....."+x);
+		x++;
+
 		return userList;
 	}
 
@@ -42,10 +52,27 @@ public class Controller {
 
 				map.put("status",-1);
 			}
-
+		System.out.println("put....."+y);
+			y++;
 			return map;
 
 		}
+
+
+	@RequestMapping(value = "admin",method = RequestMethod.GET)
+	public @ResponseBody String Say(){
+		z++;
+		System.out.println("get——admin....."+z);
+		return "This is get,is admin Pages"+z+"次!!!";
+	}
+
+
+	@RequestMapping(value = "style",method = RequestMethod.GET)
+	public @ResponseBody String stlye(){
+		p++;
+		String style="<html><head> Style 样式</head> <body><h1 >helloword</h1><p>这段文字的字体颜色为绿色</p></body></html> ";
+		return style +p+"次!!!";
+	}
 
 	}
 
