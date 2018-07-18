d9c571076c57215d1d3a618ac23c063f0aa7c49e
pitt <xct012@163.com>
2018-07-18 16:07:10 +0800

    检验相应的用例依赖，测试组依赖以内容

--- src/main/java/com/xct/testng/Suit/excelEEX.java
+++ src/main/java/com/xct/testng/Suit/excelEEX.java
@@ -5,9 +5,9 @@ import org.testng.annotations.Test;
 public class excelEEX {
     //主要为了验收相应的运行时，异常，
     //期望结果就是异常数据的内容。例如，我传入一个异常数据，为了的结果就是异常刚内容。
- @Test      //(expectedExceptions = RuntimeException.class)
+ @Test(expectedExceptions = RuntimeException.class)
     public void run1(){
-        System.out.println("这是一个运行时候异常，需要直接抛出异常");
+        System.out.println("这是一个运行时候异常，需要直接抛出异常0000001");
 
 
 
@@ -15,12 +15,45 @@ public class excelEEX {
     @Test(expectedExceptions = RuntimeException.class)
 
     public void run2(){
-        System.out.println("这是一个运行时候异常，需要直接抛出异常");
+        System.out.println("这是一个运行时候异常，需要直接抛出异常00000002");
         //手动return一个异常数据内容
 
         throw  new  RuntimeException();
 
 
+    }
+    @Test(dependsOnMethods = {"run1"}) //依赖的测试用例
+    public void run3(){
+        System.out.println("这是一个运行时候异常，需要直接抛出异常00000003");
+        //手动return一个异常数据内容
+
+        throw  new  RuntimeException();
+
+
+    }
+
+
+    //y依赖组进行测试
+    @Test(groups = "group1")
+    public void grun4(){
+        System.out.println("测试依赖内容测试！！！！！！！！！000004");
+        //手动return一个异常数据内容,
+        throw  new  RuntimeException();
+    }
+    @Test(groups = "group1")
+    public void grun5(){
+        System.out.println("测试依赖内容测试！！！！！！！！！000005");
+        //手动return一个异常数据内容
+    }
+    @Test
+    public void grun6(){
+        System.out.println("测试依赖内容测试！！！！！！！！！000006");
+        //手动return一个异常数据内容
+    }
+    @Test(dependsOnGroups = {"group1"})
+    public void grun7(){
+        System.out.println("测试依赖内容测试！！！！！！！！！000007");
+        //手动return一个异常数据内容
     }
 
 }
