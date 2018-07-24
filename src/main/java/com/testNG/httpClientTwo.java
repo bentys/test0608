091935f482b486157b8495058477ec7e234616bd
pitt <xct012@163.com>
2018-07-24 18:27:02 +0800

    添加相应的工具类

--- src/main/java/com/testNG/httpClientTwo.java
+++ src/main/java/com/testNG/httpClientTwo.java
@@ -1,72 +1,50 @@
 package com.testNG;
 
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.HttpContext;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class httpClientTwo {
-    public  static void main(String[] args){
-        HttpClient s1= new HttpClient() {
-            @Override
-            public HttpParams getParams() {
-                return null;
+    public  static void main(String[] args) throws Exception{
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpget = new HttpGet("http://localhost:8082/users");
+        CloseableHttpResponse response = httpclient.execute(httpget);
+        try {
+            HttpEntity entity = response.getEntity();
+            System.out.println("Executing request " + httpget.getRequestLine());
+            System.out.println("状态显示1"+response.getStatusLine());
+            System.out.println("状态显示2"+response.toString());
+            System.out.println("状态显示3"+response.getFirstHeader("Content-Type"));
+            System.out.println("状态显示4"+response.getEntity().getContentEncoding());
+            System.out.println(response.getStatusLine().getStatusCode());
+            System.out.println(response.getStatusLine().getReasonPhrase().toString().isEmpty());
+            System.out.println(response.getStatusLine().toString());
+            if (entity != null) {
+                InputStream instream = entity.getContent();
+                try {
+                    // do something useful
+                } finally {
+                    instream.close();
+                }
             }
-
-            @Override
-            public ClientConnectionManager getConnectionManager() {
-                return null;
-            }
-
-            @Override
-            public HttpResponse execute(HttpUriRequest request) throws IOException, ClientProtocolException {
-                return null;
-            }
-
-            @Override
-            public HttpResponse execute(HttpUriRequest request, HttpContext context) throws IOException, ClientProtocolException {
-                return null;
-            }
-
-            @Override
-            public HttpResponse execute(HttpHost target, HttpRequest request) throws IOException, ClientProtocolException {
-                return null;
-            }
-
-            @Override
-            public HttpResponse execute(HttpHost target, HttpRequest request, HttpContext context) throws IOException, ClientProtocolException {
-                return null;
-            }
-
-            @Override
-            public <T> T execute(HttpUriRequest request, ResponseHandler<? extends T> responseHandler) throws IOException, ClientProtocolException {
-                return null;
-            }
-
-            @Override
-            public <T> T execute(HttpUriRequest request, ResponseHandler<? extends T> responseHandler, HttpContext context) throws IOException, ClientProtocolException {
-                return null;
-            }
-
-            @Override
-            public <T> T execute(HttpHost target, HttpRequest request, ResponseHandler<? extends T> responseHandler) throws IOException, ClientProtocolException {
-                return null;
-            }
-
-            @Override
-            public <T> T execute(HttpHost target, HttpRequest request, ResponseHandler<? extends T> responseHandler, HttpContext context) throws IOException, ClientProtocolException {
-                return null;
-            }
-        };
-
+        } finally {
+            response.close();
+        }
     }
 }
