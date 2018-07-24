4632e9966b8fdc043064348260c6997f71965fc6
pitt <xct012@163.com>
2018-07-24 17:20:57 +0800

    添加相应的工具类

---
+++ src/main/java/com/testNG/httpClientTwo.java
@@ -0,0 +1,11 @@
+package com.testNG;
+
+
+import org.apache.http.client.HttpClient;
+
+public class httpClientTwo {
+    public  static void main(String[] args){
+        HttpClient s1= new HttpClient();
+
+    }
+}
